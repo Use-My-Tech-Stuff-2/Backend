c015cd5ade2b51bee0147926cfb4aa0118105196
jarrodskahill <jarrodskahill@gmail.com>
2020-02-05 13:42:20 -0800

    added more dummy data for front-end to play around with

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -6,7 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, username: 'test', password:'$2a$10$06O4kv2CcsBOKYj2im5wneQGH4GcLvnz.uBLTqkPPHx5Iqu8afhWi', department:'owner'},
-        {id: 2, username: 'db', password:'$2a$10$06O4kv2CcsBOKYj2im5wneQGH4GcLvnz.uBLTqkPPHx5Iqu8afhWi', department:'owner'}
+        {id: 2, username: 'db', password:'$2a$10$06O4kv2CcsBOKYj2im5wneQGH4GcLvnz.uBLTqkPPHx5Iqu8afhWi', department:'owner'},
+        {id: 3, username: 'ye', password:'$2a$10$06O4kv2CcsBOKYj2im5wneQGH4GcLvnz.uBLTqkPPHx5Iqu8afhWi', department:'owner'}
 
       ]);
     });
